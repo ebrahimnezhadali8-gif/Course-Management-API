48bf89b749dd4118ad564294b7d15a0f8de41a60
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-10-16 12:31:49 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const courseRouter = require("./router/course-router");
+const studentRouter = require("./router/student-router");
 
 app.use(express.json());
-app.use("/api/courses" , courseRouter)
+app.use("/api/courses", courseRouter);
+app.use("/api/students", studentRouter);
 
-app.get('/',(req,res)=>{
-    res.send("Course Management API is running...")
+app.get("/", (req, res) => {
+  res.send("Course Management API is running...");
 });
- 
+
 const port = process.env.PORT || 3000;
- 
- app.listen(port , ()=>{
-    console.log(`server listen in port ${port} ...!`)
- });
+
+app.listen(port, () => {
+  console.log(`server listen in port ${port} ...!`);
+});

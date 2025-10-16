dbe59aa0853500c0bee3f6f0b57213228c35c200
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-10-16 14:52:04 -0700

    Create enrollment-model.js

---
+++ models/enrollment-model.js
@@ -0,0 +1,32 @@
+const pool = require("../utilities/db");
+
+class EnrollmentModel {
+  // GET ALL ENROLLMENTS
+  static getEnrollments = async () => {
+    const [result] = await pool.query(`call get_enrollments()`);
+    return result[0];
+  };
+  // GET ENROLLMENTS WITH ID SPECIAL
+  static getEnrollmentStudent = async (id) => {
+    const [result] = await pool.query(`call get_enrollments_student(?)`, [id]);
+    return result[0];
+  };
+  //ADD ENROLLMENTS
+  static insertEnrollment = async (studentId, coursesId) => {
+    const result = await pool.query(`call add_enroll(?,?)`, [
+      studentId,
+      coursesId,
+    ]);
+    return { studentId, coursesId };
+  };
+  //DELETE ENROLLMENTS
+  static deleteEnrollment = async (studentId, coursesId) => {
+    const result = await pool.query(`call delete_enrollment(?,?)`, [
+      studentId,
+      coursesId,
+    ]);
+    return { studentId, coursesId };
+  };
+}
+
+module.exports = EnrollmentModel;

147383a06cc700c44b3941b16ac8f3d3d92b7491
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-10-15 21:18:22 -0700

    Update course-model.js

--- models/course-model.js
+++ models/course-model.js
@@ -1,53 +1,44 @@
 const pool = require("../utilities/db");
 
 class CourseModel {
+//GET ALL COURSES
   static getCourses = async () => {
     const [result] = await pool.query("call get_courses(null); ");
     return result[0];
   };
+//GET COURSE WITH ID SPECIAL
   static getCourse = async (id) => {
     const [result] = await pool.query("call get_courses(?)", [id]);
     return result[0];
   };
+//GET COURSE WITH NAME SPECIAL
+  static getCourseName = async (name) => {
+    const [result] = await pool.query(`select * from courses where name =?`, [
+      name,
+    ]);
+    return result;
+  };
+//ADD COURSE
   static insertCourses = async (name, description) => {
     const result = await pool.query(`call add_course(?,?) `, [
       name,
       description,
     ]);
-    return getCourses();
   };
-
+//UPDATE COURSE (NAME AND DESCRIPTION)
   static updateCourses = async (id, name, description) => {
     const result = await pool.query(`call update_course(?,?,?)`, [
       id,
       name,
       description,
     ]);
-    return getcourse(id);
+    return this.getCourse(id);
   };
-
+//DELETE COURSE 
   static deleteCourses = async (id) => {
     const result = await pool.query(`call delete_course(?)`, [id]);
     return id;
   };
 }
 
-/*(async()=>{
-
-    console.log("All courses ........")
-    await CourseModel.getCourses().then((result)=> console.log(result));
-
-    console.log("Course with specific id ..........")
-    await CourseModel.getcourse(1).then((result)=> console.log(result));
-
-    console.log('Add course ........')
-    await CourseModel.insertCourses('java','Learning java').then((result)=> console.log(result));
-
-    console.log("update course .......")
-    await CourseModel.updateCourses(77 , 'c++' , 'Leaning c++ ...').then((result)=> console.log(result));
-
-    console.log("delete course ......")
-    await CourseModel.deleteCourses(91).then((result)=> console.log(result));
-})();
-*/
 module.exports = CourseModel;

0e74f57ca49e2016c06c6141253836635fd6a591
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-10-16 12:32:25 -0700

    Update course-router.js

--- router/course-router.js
+++ router/course-router.js
@@ -1,15 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const courseController = require('../controller/course-controller');
+const courseController = require("../controller/course-controller");
 
-router.get("/:id" ,courseController.getcourse);
+router.get("/:id", courseController.getcourse);
+router.get("/", courseController.getCourses);
+router.post("/", courseController.postCourse);
+router.put("/:id", courseController.putCourse);
+router.delete("/:id", courseController.deleteCourses);
 
-router.get("/" ,courseController.getCourses);
-
-router.post("/" , courseController.postCourse);
-
-router.put("/:id" , courseController.putCourse);
-
-router.delete("/:id" , courseController.deleteCourses);
-
-module.exports = router
+module.exports = router;

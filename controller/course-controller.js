4d4f4f19426f4702511d83a8088938a416477e25
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-10-19 16:35:37 -0700

    Update course-controller.js

--- controller/course-controller.js
+++ controller/course-controller.js
@@ -1,5 +1,8 @@
+const CourseModel = require("../models/course-model");
 const coursesModule = require("../models/course-model");
 const EnrollmentModel = require("../models/enrollment-model");
+const Joi = require("joi");
+const _ = require("lodash");
 
 const getcourse = (req, res) => {
   //Checking the existence of the course
@@ -13,52 +16,61 @@ const getCourses = (req, res) => {
     res.send(result);
   });
 };
-const postCourse = (req, res) => {
-  const { name, description } = req.body;
+const postCourse = async (req, res) => {
+  let { name, description } = req.body;
   //Checking the existence of the name or not less than three characters
-  if (!name || name.length < 3) {
-    return res.status(400).send("name is required!");
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    description: Joi.string().allow("").max(255),
+  };
+  const validateResult = Joi.object(schema).validate(req.body);
+  if (validateResult.error) {
+    return res.send(validateResult.error.details[0].message);
   }
-  coursesModule.getCourseName(name).then((result) => {
-    if (!result) return res.status(409).send("courses whith id not found");
-  });
-  if (!description) description = "";
+  const course = await coursesModule.getCourseName(name);
+  if (course) return res.status(400).send("Course already registered");
 
-  coursesModule.insertCourses(name, description).then((result) => {
-    res.send(
-      `Course added successfully name:${name} and description:${description}`
-    );
-  });
+  const result = await coursesModule.insertCourses(name, description);
+  const newCourse = await coursesModule.getCourseName(name);
+  res.send(_.pick(newCourse, ["name", "description", "date"]));
 };
-const putCourse = (req, res) => {
-  const { name, description } = req.body;
-  if (!name || name.length < 3) {
-    return res.status(400).send("name is required!");
+const putCourse = async (req, res) => {
+  let { name, description } = req.body;
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    description: Joi.string().allow("").max(255),
+  };
+  const validateResult = Joi.object(schema).validate(req.body);
+  if (validateResult.error) {
+    return res.send(validateResult.error.details[0].message);
   }
-  coursesModule.getCourseName(name).then((result) => {
-    if (!result) return res.status(404).send("courses whith id not found");
-  });
-  if (!description) description = "";
-  coursesModule
-    .updateCourses(parseInt(req.params.id), name, description)
-    .then((result) => {
-      res.send(result);
-    });
+  const course = await coursesModule.getCourse(parseInt(req.params.id));
+
+  if (!course) return res.status(404).send("courses whith id not found");
+
+  const result = await coursesModule.updateCourses(
+    parseInt(req.params.id),
+    name,
+    description
+  );
+  const upCourse = await CourseModel.getCourse(parseInt(req.params.id));
+  console.log(upCourse);
+  res.send(_.pick(upCourse[0], ["name", "description"]));
 };
-const deleteCourses = (req, res) => {
-  coursesModule.getCourse(parseInt(req.params.id)).then((result) => {
-    if (!result) return res.status(404).send("courses whith id not found");
-  });
+const deleteCourses = async (req, res) => {
+  const course = await coursesModule.getCourse(parseInt(req.params.id));
+  if (!course) return res.status(404).send("courses whith id not found");
+
   //check table Enrollments
-  EnrollmentModel.getEnrollmentStudent(req.params.id).then((result) => {
-    if (result)
-      return res
-        .status(409)
-        .send("Cannot delete course. They are enrolled in a student.");
-  });
-  coursesModule.deleteCourses(parseInt(req.params.id)).then((result) => {
-    res.send(`Course with id ${req.params.id} deleted `);
-  });
+  const enrollment = await EnrollmentModel.getEnrollmentStudent(req.params.id);
+  if (enrollment[0])
+    return res
+      .status(409)
+      .send("Cannot delete course. They are enrolled in a student.");
+
+  const deleted = await coursesModule.deleteCourses(parseInt(req.params.id));
+
+  res.send(`Course with id ${req.params.id} deleted `);
 };
 module.exports = {
   getcourse,

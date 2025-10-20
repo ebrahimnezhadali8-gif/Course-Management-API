388d2121277f79f074e22072443c01df347aebbd
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-10-19 19:24:32 -0700

    Update enrollment-controller.js

--- controller/enrollment-controller.js
+++ controller/enrollment-controller.js
@@ -13,34 +13,57 @@ const getEnrollmentStudent = (req, res) => {
     res.send(result);
   });
 };
-const postEnrollment = (req, res) => {
+const postEnrollment = async (req, res) => {
   const { studentId, courseId } = req.params;
   //check id student and id course
   if (!studentId || !courseId)
     return res.status(400).send("student_id and course_id are required.");
   // check course in table courses
-  StudentModel.getstudent(studentId).then((result) => {
-    if (!result) return res.status(409).send("Student not found.");
-  });
+  const student = await StudentModel.getStudent(studentId);
+  if (!student) return res.status(404).send("Student not found.");
+
   //check student in table students
-  CourseModel.getCourse(courseId).then((result) => {
-    if (!result) return res.status(409).send("Course not found.");
-  });
-  EnrollmentModel.insertEnrollment(studentId, parseInt(courseId)).then(
-    (result) => {
-      res.send(`added successfully!`);
-    }
+  const course = await CourseModel.getCourse(courseId);
+  if (!course) return res.status(404).send("Course not found.");
+
+  //check student with course table Enrollment
+  const enrollment = await EnrollmentModel.getEnrollmentCourseStudent(
+    studentId,
+    parseInt(courseId)
   );
+  console.log(enrollment);
+  if (enrollment[0])
+    return res.status(404).send("studend with Course registered.");
+  //add enrollment
+  const result = await EnrollmentModel.insertEnrollment(
+    studentId,
+    parseInt(courseId)
+  );
+  const newEnrollment = await EnrollmentModel.getEnrollmentCourseStudent(
+    studentId,
+    parseInt(courseId)
+  );
+  res.send(newEnrollment[0]);
 };
-const deleteEnrollment = (req, res) => {
+const deleteEnrollment = async (req, res) => {
   const { studentId, courseId } = req.params;
   if (!studentId || !courseId)
     return res.status(400).send("student_id and course_id are required.");
-  EnrollmentModel.deleteEnrollment(studentId, parseInt(courseId)).then(
-    (result) => {
-      res.send("deleted successfully!");
-    }
+
+  //check student with course table Enrollment
+  const enrollment = await EnrollmentModel.getEnrollmentCourseStudent(
+    studentId,
+    parseInt(courseId)
   );
+  if (!enrollment[0])
+    return res.status(404).send("studend with Course not found.");
+
+  const deleted = await EnrollmentModel.deleteEnrollment(
+    studentId,
+    parseInt(courseId)
+  );
+
+  res.send(`deleted successfully student name : ${enrollment[0].name} in course : ${enrollment[0].course}!`);
 };
 
 module.exports = {

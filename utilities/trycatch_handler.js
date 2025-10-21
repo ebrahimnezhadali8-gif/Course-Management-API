21227c548b73f54d6a98cc744a34b029249a46f6
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-10-20 17:59:20 -0700

    Create trycatch_handler.js

---
+++ utilities/trycatch_handler.js
@@ -0,0 +1,10 @@
+exports.trycatchHandler = (controller) => {
+  return async (req, res, next) => {
+    try {
+      await controller(req, res);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+

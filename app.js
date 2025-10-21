e9350b2679060b5e1ae2524a3917eb5c3479ea6a
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-10-20 17:58:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require("express");
 const app = express();
 const courseRouter = require("./router/course-router");
 const studentRouter = require("./router/student-router");
-const enrollmentRouter =require("./router/enrollment-router");
+const enrollmentRouter = require("./router/enrollment-router");
+const errorHandler = require("./middlware/error_handler");
 
 app.use(express.json());
 app.use("/api/courses", courseRouter);
 app.use("/api/students", studentRouter);
-app.use("/api/enrollment" , enrollmentRouter);
+app.use("/api/enrollment", enrollmentRouter);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("Course Management API is running...");

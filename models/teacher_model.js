4cdf519c8eeaa9defafecbdd23ade2373aa4951e
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-11-03 19:12:42 -0800

    Update teacher_model.js

--- models/teacher_model.js
+++ models/teacher_model.js
@@ -8,6 +8,28 @@ class TeacherModel {
     );
     return result[0];
   };
+  //id teacher with id course
+  static getTeacherInCourse = async (teacherId, courseId) => {
+    const [result] = await pool.query(`call get_teacher_by_course(?,?)`, [
+      teacherId,
+      courseId,
+    ]);
+    return result[0];
+  };
+  static insertTeacherCourse = async (teacherId, courseId) => {
+    const result = await pool.query(`call create_teacher_course(?,?)`, [
+      teacherId,
+      courseId,
+    ]);
+    return result;
+  };
+  static deleteTeacherCourse = async (teacherId, courseId) => {
+    const result = await pool.query(`call delete_teacher_course(?,?)`, [
+      teacherId,
+      courseId,
+    ]);
+    return result;
+  };
 }
 
 module.exports = TeacherModel;

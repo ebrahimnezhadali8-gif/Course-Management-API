53bee1afe14b31484dd6b453b3ef429c6a4255ec
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-11-03 19:04:50 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const courseRouter = require("./router/course-router");
-const enrollmentRouter = require("./router/enrollment-router");
 const authRouter = require("./router/auth-router");
 const errorHandler = require("./middlware/error_handler");
 const userRoute = require("./router/user_router");
 const teacherRoute = require("./router/teacher_router");
+const studentRouter = require("./router/student_course");
 
 app.use(express.json());
 app.use("/api/courses", courseRouter);
-app.use("/api/enrollment", enrollmentRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRoute);
 app.use("/api/teacher", teacherRoute);
+app.use("/api/student", studentRouter);
 app.use(errorHandler);
 
 app.get("/", (req, res) => {

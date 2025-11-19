329c81fa0fd1f9c8760fe9959b52e3f836627b38
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-11-19 09:36:15 -0800

    Update error_handler.js

--- back_end/middlware/error_handler.js
+++ back_end/middlware/error_handler.js
@@ -4,7 +4,7 @@ const logger = require("../utilities/winston_loger");
 const errorHandler = (error, req, res, next) => {
   if (error.name === "ValidationError") {
     logger.warn(`Validation failed: ${error.message}`);
-    return res.status(400).send("validation is failed");
+    return res.status(400).send({message : "Some fields are invalid. Please check your input."});
   }
   if (error instanceof AppError) {
     logger.error(
